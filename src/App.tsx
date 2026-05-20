5f217f6e10020d708769e50e378beab9c0ff8eee
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2026-05-20 10:25:46 +0530

    add shadcn and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import {Button} from "./components/ui/button";
+
 function App() {
   return (
-    <div>
-      <div>
-        <p>AIByte Dashboard + Landing Page</p>
+    <div className="h-screen w-full flex items-center justify-center">
+      <div className="h-[400px] w-[60%] flex items-center justify-center flex-col gap-10 rounded-md shadow-md">
+        <p className="text-3xl font-bold">AIByte Dashboard + Landing Page</p>
+        <Button>Click Me</Button>
       </div>
     </div>
   );
